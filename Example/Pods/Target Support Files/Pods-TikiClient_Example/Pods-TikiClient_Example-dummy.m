39daa457c2d5e6eabdcca2ec13bbb706ea9f2480
Ricardo Gonçalves <ricardobrg@users.noreply.github.com>
2024-01-18 10:49:26 -0300

    initial

---
+++ Example/Pods/Target Support Files/Pods-TikiClient_Example/Pods-TikiClient_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TikiClient_Example : NSObject
+@end
+@implementation PodsDummy_Pods_TikiClient_Example
+@end
---
+++ Example/Pods/Target Support Files/TikiClient/TikiClient-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_TikiClient : NSObject
+@end
+@implementation PodsDummy_TikiClient
+@end
