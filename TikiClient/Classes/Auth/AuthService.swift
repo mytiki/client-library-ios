257f58058eac464c79f1c9e824a9499c020104cb
Jessé Monteiro <jessemonteiroferreira@gmail.com>
2024-02-22 19:15:23 -0300

    feat/Create the user device address

--- TikiClient/Classes/Auth/AuthService.swift
+++ TikiClient/Classes/Auth/AuthService.swift
@@ -4,6 +4,10 @@
  */
 
 import Foundation
+import Security
+import CryptoKit
+import CryptoSwift
+
 
 /// Authentication Service for TIKI
 public class AuthService {
@@ -38,5 +42,54 @@ public class AuthService {
     /// - Returns: The updated authentication token.
     public func refresh() -> String {
         return ""
+    }
+    
+ 
+    // Method to generate a key pair
+    public static func generateKeyPair() -> (publicKey: SecKey, privateKey: SecKey)? {
+        var publicKey, privateKey: SecKey?
+        
+        let attributes: [String: Any] = [
+            kSecAttrKeyType as String: kSecAttrKeyTypeRSA,
+            kSecAttrKeySizeInBits as String: 2048,
+            kSecPrivateKeyAttrs as String: [
+                kSecAttrIsPermanent as String: true,
+                kSecAttrApplicationTag as String: "com.example.TikiKeyAlias",
+                kSecAttrAccessControl as String: SecAccessControlCreateWithFlags(
+                    nil,
+                    kSecAttrAccessibleWhenUnlockedThisDeviceOnly,
+                    [],
+                    nil
+                )!
+            ]
+        ]
+        
+        var error: Unmanaged<CFError>?
+        guard let privateKey = SecKeyCreateRandomKey(attributes as CFDictionary, &error) else {
+            print("Error generating key pair: \(error.debugDescription)")
+            return nil
+        }
+        
+        publicKey = SecKeyCopyPublicKey(privateKey)
+        
+        return (publicKey!, privateKey)
+    }
+
+    // Method to create the address using SHA-256 digest
+    public static func address(publicKey: SecKey) -> String? {
+        do {
+            // Get the public key data
+            guard let publicKeyData = SecKeyCopyExternalRepresentation(publicKey, nil) as Data? else {
+                return nil
+            }
+
+            print(publicKeyData.base64EncodedString())
+            
+            // Create SHA-256 digest of the public key
+            let digest = publicKeyData.sha3(.sha256)
+            print(digest)
+
+            return Data(digest).base64EncodedString()
+        }
     }
 }
