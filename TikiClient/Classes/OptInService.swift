2b79f2784285b44ea8d6b40b1b3f26f9038f626a
Jessé Monteiro <jessemonteiroferreira@gmail.com>
2024-05-30 15:18:16 -0300

    Create ShowOffer Method

--- TikiClient/Classes/OptInService.swift
+++ TikiClient/Classes/OptInService.swift
@@ -7,10 +7,23 @@ import Foundation
 
 
 public class OptInService {
-    public static func showOffer(){
-        
+    public static var offer: Offer?
+    public static var step: OfferFlowStep?
+    public static var offerList: [Offer]?
+    
+    public static func showOffer(offer: Offer){
+        OptInService.step = .offers
+        for permission in offer.permissions {
+            if(!(permission?.isAuthorized())!){
+                OptInService.step = .permissions
+                permission?.requestAuth()
+            }
+        }
+        OptInService.offer = offer
     }
-    public static func showSettings(){
-        
+    
+    public static func showSettings(offers: [Offer]){
+        OptInService.step = .settings
+        OptInService.offerList = offers
     }
 }
